7477d834d98b591c1bf55c071e67e66329184ccb
NikolayMiroshnichenko <36457433+NikolayMiroshnichenko@users.noreply.github.com>
2021-10-06 23:31:21 +0300

    creat gallery

--- js/slider.js
+++ js/slider.js
@@ -19,3 +19,43 @@ const apartment = new Swiper(".apartmentSlider", {
         },
     },
 });
+
+const gallerySlider = new Swiper(".gallerySlider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        1220: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    },
+})
+
+// window.addEventListener('resize', () => {
+//     if (window.screen.width > 1220) return;
+
+
+//     console.log("HIIII")
+// });
+
+{/*  */}
+
+
+
+
+
+
+
+
